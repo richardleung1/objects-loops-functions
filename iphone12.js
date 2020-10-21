378e8f906fb53d92c9bc02e611baf35557a2a50f
Richard Leung <michelleli@MMac.local>
2020-10-21 15:39:12 -0400

    [object] Add objects to files

---
+++ iphone12.js
@@ -0,0 +1,11 @@
+const iphone12 = {
+    image: "path to where the image is",
+    length: "11.28",
+    title: "iPhone 12 unboxing experience + Magsafe",
+    username: "Marques Brownlee",
+    views: "5.5M",
+    uploaded: "15 hours ago",
+    length: "11:28"
+}
+console.log(iphone12.title)
+console.log(iphone12.length);
---
+++ star-wars.js
@@ -0,0 +1,10 @@
+const starWars = {
+    title: "Special Look | The Mandalorian | Disney+",
+    author: 'Star Wars',
+    studio: "Disney+ Original",
+    views:  "1M",
+    uploaded: '1 day ago',
+    length: "1:00"
+}
+console.log(starWars.title)
+console.log(starWars.views);
